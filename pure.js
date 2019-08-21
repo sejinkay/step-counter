bf57a14b570128c69e81539ecdc8cdc1ab778b5c
sejinkay <sejinkay@gmail.com>
2019-08-21 23:45:27 +0100

    played with h1 tag, added querySelectorAll

--- pure.js
+++ pure.js
@@ -1,4 +1,14 @@
 
-const headerTag = document.querySelector("h1")
+// const headerTag = document.querySelector("h1")
 
-headerTag.innerHTML = "Hello someone"
+// headerTag.innerHTML = "Hello someone"
+
+// headerTag.style.backgroundColor = "red"
+
+// headerTag.style.fontSize = "100px"
+
+const headerTags = document.querySelectorAll("h1")
+
+headerTags.forEach(h1 => {
+	h1.style.backgroundColor = "var(--secondary-blue)"
+})
